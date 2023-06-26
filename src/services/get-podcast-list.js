a4212b706e04475bb4b8a00ad82bb73c80e3ac69
mauro soler <javiermauriciosoler@gmail.com>
2023-06-26 23:52:46 +0200

    feature: create request get podcasts

---
+++ src/services/get-podcast-list.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const fetchPodcasts = async () => {
+  try {
+    const response = await axios.get(
+      "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+    );
+    const data = response.data.feed.entry;
+    return data;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw error;
+  }
+};
+
+export default fetchPodcasts;

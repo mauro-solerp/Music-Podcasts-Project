2de6b1705c0ea061e59624f040fa235c6b1aaed1
mauro soler <javiermauriciosoler@gmail.com>
2023-06-27 10:51:21 +0200

    feature: create page Header component

--- src/components/header/PageHeader.jsx
+++ src/components/header/PageHeader.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-
+import { useNavigate } from "react-router-dom";
+import "./PageHeader.css";
 
 const PageHeader = () => {
+  let navigate = useNavigate(); 
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div style={{ width: "100%" }}>
-      <div style={{ textAlign: "initial" }}>
+      <div style={{ textAlign: "initial" }} onClick={handleClick} >
         <Typography variant="h5" component="div" noWrap>
-          Podcaster
+          <span className="header-text">Podcaster</span>
         </Typography>
       </div>
     </div>

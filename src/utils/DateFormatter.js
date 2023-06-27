099b7a1c753e779c5fda0e7c15489760bb0bf0fc
mauro soler <javiermauriciosoler@gmail.com>
2023-06-27 10:56:57 +0200

    feature: utils for show correct data functions

---
+++ src/utils/DateFormatter.js
@@ -0,0 +1,11 @@
+export default function convertDateFormat(dateString) {
+  const date = new Date(dateString);
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth() + 1;
+  const year = date.getUTCFullYear();
+
+  const formattedDay = day < 10 ? `0${day}` : day;
+  const formattedMonth = month < 10 ? `0${month}` : month;
+
+  return `${formattedDay}/${formattedMonth}/${year}`;
+}

62dfec403926b72165a6ef7f4e936833501df961
mauro soler <javiermauriciosoler@gmail.com>
2023-06-27 19:45:37 +0200

    feature: adding data permanence

--- src/services/get-podcast-list.js
+++ src/services/get-podcast-list.js
@@ -2,11 +2,21 @@ import axios from "axios";
 
 const fetchPodcasts = async () => {
   try {
-    const response = await axios.get(
-      "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
-    );
-    const data = response.data.feed.entry;
-    return data;
+    const lastRequestTime = localStorage.getItem("podcastsLastRequestTime");
+    const currentTime = new Date().getTime();
+
+    if (!lastRequestTime || currentTime - lastRequestTime > 24 * 60 * 60 * 1000) {
+      const response = await axios.get(
+        "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+      );
+      const data = response.data.feed.entry;
+
+      localStorage.setItem("podcastsData", JSON.stringify(data));
+      localStorage.setItem("podcastsLastRequestTime", currentTime);
+    }
+
+    const storedData = localStorage.getItem("podcastsData");
+    return storedData ? JSON.parse(storedData) : [];
   } catch (error) {
     console.error(error);
     throw error;

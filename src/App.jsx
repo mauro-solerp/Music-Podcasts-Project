4e1cb9f1da6424fbc93392d8c38958c587e8bbc9
mauro soler <javiermauriciosoler@gmail.com>
2023-06-27 19:46:33 +0200

    refactor: remove unnecessary provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import PodcastList from "./pages/home/PodcastList";
 import PodcastDetails from "./pages/podcast-details/PodcastDetails";
-import { PodcastProvider } from "./hooks/PodcastContext";
+import PodcastViewer from "./pages/podcast-viewer/PodcastViewer";
 
 function App() {
   return (
     <div className="App">
-      <PodcastProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<PodcastList />} />
-            <Route path="/podcast/:podcastId" element={<PodcastDetails />} />
-          </Routes>
-        </BrowserRouter>
-      </PodcastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PodcastList />} />
+          <Route path="/podcast/:podcastId" element={<PodcastDetails />} />
+          <Route
+            path="/podcast/:podcastId/episode/:episodeId"
+            element={<PodcastViewer />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

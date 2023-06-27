28987462a194c192f9ce3c597258b58d4f39c640
mauro soler <javiermauriciosoler@gmail.com>
2023-06-27 19:47:40 +0200

    refactor: change  detail petition

--- src/components/cards/podcast-details/CardDetails.jsx
+++ src/components/cards/podcast-details/CardDetails.jsx
@@ -1,49 +1,61 @@
 import { Card, Divider } from "@mui/material";
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
-import PodcastContext from "../../../hooks/PodcastContext";
+import fetchPodcasts from "../../../services/get-podcast-list";
 
 const CardDetails = (idPodcast) => {
-  const podcasts = useContext(PodcastContext);
-  const podcast = podcasts.find(
-    (podcast) => podcast.id.attributes["im:id"] === idPodcast.id
-  );
+  const [podcast, setPodcast] = useState(null);
+
+  useEffect(() => {
+    fetchPodcastData(idPodcast.id);
+  }, [idPodcast]);
+
+  const fetchPodcastData = async (podcastId) => {
+    try {
+      const response = await fetchPodcasts();
+      const podcastData = response.find(
+        (podcast) => podcast.id.attributes["im:id"] === podcastId
+      );
+      setPodcast(podcastData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
-      {
-        podcast ? ( <Card style={{ padding: "5px", maxWidth: "500px" }}>
-        <img
-          style={{ marginTop: "10px" }}
-          src={podcast["im:image"][2].label}
-          alt="Descripción de la imagen"
-        />
-        <Divider
-          variant="middle"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        />
-        <div style={{ textAlign: "start", padding: "10px" }}>
-          <Typography variant="h6" component="div" noWrap fontWeight={600}>
-            {podcast["im:name"].label}
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary">
-            By: {podcast["im:artist"].label}
-          </Typography>
+      {podcast ? (
+        <Card style={{ padding: "5px", maxWidth: "500px" }}>
+          <img
+            style={{ marginTop: "10px" }}
+            src={podcast["im:image"][2].label}
+            alt="Descripción de la imagen"
+          />
           <Divider
             variant="middle"
             style={{ marginTop: "10px", marginBottom: "10px" }}
           />
-          <Typography variant="h6" component="div" noWrap fontWeight={600}>
-            description:
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-            {podcast["summary"].label}
-          </Typography>
-        </div>
-      </Card> ) : ( null )
-      }
-      
+          <div style={{ textAlign: "start", padding: "10px" }}>
+            <Typography variant="h6" component="div" noWrap fontWeight={600}>
+              {podcast["im:name"].label}
+            </Typography>
+
+            <Typography variant="body1" color="text.secondary">
+              By: {podcast["im:artist"].label}
+            </Typography>
+            <Divider
+              variant="middle"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            />
+            <Typography variant="h6" component="div" noWrap fontWeight={600}>
+              description:
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {podcast["summary"].label}
+            </Typography>
+          </div>
+        </Card>
+      ) : null}
     </div>
   );
 };
--- src/pages/home/PodcastList.jsx
+++ src/pages/home/PodcastList.jsx
@@ -1,19 +1,38 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useEffect, useState } from "react";
 import { CircularProgress, Grid } from "@mui/material";
 import PodcastCard from "../../components/cards/podcast-card/PodcastCard";
 import "./PodcastList.css";
 import FilterLabel from "../../components/label/FilterLabel";
 import PageHeader from "../../components/header/PageHeader";
-import PodcastContext from "../../hooks/PodcastContext";
+
+import fetchPodcasts from "../../services/get-podcast-list";
 
 const PodcastList = () => {
-  const podcasts = useContext(PodcastContext); // Accede a los datos de los podcasts desde el contexto
+  const [podcasts, setPodcasts] = useState([]);
   const [filteredPodcasts, setFilteredPodcasts] = useState([]);
 
   useEffect(() => {
-    setFilteredPodcasts(podcasts); // Actualiza los podcasts filtrados con los datos del contexto
-  }, [podcasts]);
+    fetchPodcastsData(); 
+  }, []);
+
+  const fetchPodcastsData = async () => {
+    try {
+      const storedData = localStorage.getItem("podcastsData");
+      
+      if (storedData) {
+        const data = JSON.parse(storedData);
+        setPodcasts(data);
+        setFilteredPodcasts(data);
+      } else {
+        const response = await fetchPodcasts();
+        setPodcasts(response);
+        setFilteredPodcasts(response);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
